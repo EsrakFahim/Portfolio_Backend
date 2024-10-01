4a31eed2d63cf67327962f9d18c0ba63ac8fdf39
EsrakFahim <efahim0656@gmail.com>
2024-10-01 23:10:15 +0600

    Update email configuration in mailSender.js

--- src/Services/mailSender.js
+++ src/Services/mailSender.js
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_SERVICE, // mail.privateemail.com for Namecheap
       port: Number(process.env.EMAIL_PORT), // Ensure this is a number
-      secure: process.env.EMAIL_SECURE === "true", // true for SSL, false for TLS
+      secure: process.env.EMAIL_SECURE , // true for SSL, false for TLS
       auth: {
             user: process.env.EMAIL_ACCOUNT, // Your Namecheap email
             pass: process.env.EMAIL_PASSWORD, // Your email password

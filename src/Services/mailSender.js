ff005a2ad537b90b05abbb3bb2683d8a5ac9b714
EsrakFahim <efahim0656@gmail.com>
2024-11-25 22:29:03 +0600

    Refactor mail sender configuration to improve comment clarity in mailSender.js

--- src/Services/mailSender.js
+++ src/Services/mailSender.js
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
       service: process.env.EMAIL_SERVICE, // service name
       auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS,
+            user: process.env.EMAIL_USER,// email
+            pass: process.env.EMAIL_PASS, // password
       },
 });
